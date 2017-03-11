d2c5745e85f9140dc471ac2c40bb2af81afe166f
fabiendibot <fabien.dibot@gmail.com>
2017-03-11 10:18:27 +0100

    invoking magic

--- Tests/Appveyor.ps1
+++ Tests/Appveyor.ps1
@@ -7,8 +7,13 @@ New-Item -Path "C:\Program Files\WindowsPowerShell\Modules" -Name "cMicrosoftUpd
 New-Item -Path "C:\Program Files\WindowsPowerShell\Modules\cMicrosoftUpdate" -Name "0.0.1" -Force -ItemType Directory
 Copy-Item -Path "C:\projects\cmicrosoftupdate\*" -Destination "C:\Program Files\WindowsPowerShell\Modules\cMicrosoftUpdate"
 
-Get-DscResource
+ls "C:\Program Files\WindowsPowerShell\Modules" -Recurse
 
+Get-module -ListAvailable
+
+Import-module -name cMicrosoftUpdate
+
+Get-DscResource
 
 "`n`tPerforming DSC Configuration`n"
 
