371a389cba768884bc68b3ea6307cccf00b3e357
fabiendibot <fabien.dibot@gmail.com>
2017-03-11 09:58:20 +0100

    replacing PSModulePath

--- Tests/Appveyor.ps1
+++ Tests/Appveyor.ps1
@@ -2,6 +2,8 @@
 # This is a simple proof of concept
 $PSModuleAutoloadingPreference = "none"
 
+$env:PSModulePath = "C:\Program Files\WindowsPowerShell\Modules;C:\Windows\system32\WindowsPowerShell\v1.0\Modules"
+
 New-Item -Path "C:\Program Files\WindowsPowerShell\Modules" -Name "cMicrosoftUpdate" -Force -ItemType Directory
 New-Item -Path "C:\Program Files\WindowsPowerShell\Modules\cMicrosoftUpdate" -Name "0.0.1" -Force -ItemType Directory
 Copy-Item -Path "C:\projects\cmicrosoftupdate\*" -Destination "C:\Program Files\WindowsPowerShell\Modules\cMicrosoftUpdate\0.0.1"
