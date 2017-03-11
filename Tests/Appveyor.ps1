bc89e7d283887c9cc4d473d384274b63645b19d3
fabiendibot <fabien.dibot@gmail.com>
2017-03-11 10:27:08 +0100

    add refresh env

--- Tests/Appveyor.ps1
+++ Tests/Appveyor.ps1
@@ -1,6 +1,8 @@
 # This script will invoke a DSC configuration
 # This is a simple proof of concept
 
+refreshenv
+
 New-Item -Path "C:\Windows\system32\WindowsPowerShell\v1.0\Modules" -Name "cMicrosoftUpdate" -Force -ItemType Directory
 #New-Item -Path "C:\Program Files\WindowsPowerShell\Modules\cMicrosoftUpdate" -Name "0.0.1" -Force -ItemType Directory
 Copy-Item -Path "C:\projects\cmicrosoftupdate\*" -Destination "C:\Windows\system32\WindowsPowerShell\v1.0\Modules\cMicrosoftUpdate"
