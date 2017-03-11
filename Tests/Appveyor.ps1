77216888249dea2b5b069b4b9f3d240c1c670a43
fabiendibot <fabien.dibot@gmail.com>
2017-03-11 04:11:52 +0100

    Je suis un gros boulet

--- Tests/Appveyor.ps1
+++ Tests/Appveyor.ps1
@@ -2,8 +2,8 @@
 # This is a simple proof of concept
 
 
-New-Item -Path "C:\Program Files\WindowsPowerShell\Modules" -Name "cMicrosoftUpdate" -Force
-New-Item -Path "C:\Program Files\WindowsPowerShell\Modules\cMicrosoftUpdate" -Name "0.0.1" -Force
+New-Item -Path "C:\Program Files\WindowsPowerShell\Modules" -Name "cMicrosoftUpdate" -Force -ItemType Directory
+New-Item -Path "C:\Program Files\WindowsPowerShell\Modules\cMicrosoftUpdate" -Name "0.0.1" -Force -ItemType Directory
 Copy-Item -Path "C:\projects\cmicrosoftupdate\*" -Destination "C:\Program Files\WindowsPowerShell\Modules\cMicrosoftUpdate\0.0.1"
 
 "`n`tPerforming DSC Configuration`n"
